eb2380002590a7f3d1c17798aa98de3a45427bb9
Teklu Mezgebo <mezge002@umn.edu>
2023-01-20 16:23:45 -0600

    commit tree 3a3bbff5278cd5a5b0cc8f6ef79a53c6f752cbd5

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(acitivity = 'roller-skate') {
+    return 'This Saturday, I want to ' + acitivity + '!'
+}
+
+function mondayWork(work = 'go to the office') {
+    return 'This Monday, I will ' + work + '.'
+}
+
+function wrapAdjective(special = '*'){
+    return function(statement){
+        return 'You are ' + special + statement + special + '!'
+    }
+}
